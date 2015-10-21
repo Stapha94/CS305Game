319c234d9aa94148201a6bdf87363456f3d79ac4
Paul Fox <pfoxiii@gmail.com>
2015-10-21 19:03:21 -0400

    First Iteration of a Working Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
   def create
     @facilitator = Facilitator.find_by_email(params[:session][:email])
-    if @facilitator && @facilitator.authenticate(params[:session][:passkey])
-      session[:user_id] = @facilitator.id
+    if @facilitator && @facilitator.authenticate(params[:session][:password])
+      session[:facilitator_id] = @facilitator.id
       redirect_to '/facilitator'
     else
       redirect_to '/facilitator/login'
     end
   end
   def destroy
-    session[:user_id] = nil
+    session[:facilitator_id] = nil
     redirect_to '/'
   end
 end
